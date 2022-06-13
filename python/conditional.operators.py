d689b181c496b8e49cbcf17845c692f5a3a986d6
lin6echo <bajzathcsaba@gmail.com>
2022-06-13 21:02:51 +0200

    1

---
+++ python/conditional.operators.py
@@ -0,0 +1,12 @@
+# if condition:
+# do this /if is it true
+# else:
+# do this /execute if the condition is false
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
---
+++ python/odd.or.even.py
@@ -0,0 +1,6 @@
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0: #comparison operator ==
+    print("This is an even number.")
+else:
+    print("Tis is an odd number.")
