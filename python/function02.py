32cf10ada580c85211e6babb138a6f494eaead48
lin6echo <bajzathcsaba@gmail.com>
2022-06-17 15:53:56 +0200

    1

---
+++ python/function02.py
@@ -0,0 +1,18 @@
+# Built in function: print(), len()
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# own function
+def my_function(): # def - keyword
+    print("Hello")
+    print("Bye")
+
+my_function() # call the function
+
+def my_function(): # Define the function
+    #Do this
+    #Then do this
+    #Finally do this
+
+    my_function() # calling the function
---
+++ python/function03.py
@@ -0,0 +1,28 @@
+# Simple function
+
+# def greet():
+#     print("Hello")
+#     print("How do you do?")
+#     print("Isn't the weather nice today?")
+
+# greet()
+
+# Function that allows for input
+
+# def greet_With_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+
+# greet_With_name("Csaba")
+
+# Function with more than 1 input
+
+def greet_with(name, location):
+     print(f"Hello {name}")
+     print(f"What is it like in {location}")
+
+# greet_with("Jack Bauer", "nowhere")
+
+# Function with keyword arguments
+
+greet_with(name="Angela", location="Eger")
