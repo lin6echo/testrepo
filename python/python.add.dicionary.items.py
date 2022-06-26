7f49fb81e2df4ebfb19c5080c6a4012415fffaf8
lin6echo <bajzathcsaba@gmail.com>
2022-06-26 21:34:51 +0200

    1

---
+++ python/python.add.dicionary.items.py
@@ -0,0 +1,26 @@
+# Adding Items
+
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+# Update Dictionary
+
+# The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
+
+# The argument must be a dictionary, or an iterable object with key:value pairs.
+
+# Add a color item to the dictionary by using the update() method:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
---
+++ python/python.loop.dictionaries.py
@@ -0,0 +1,36 @@
+# Loop Through a Dictionary
+
+# You can loop through a dictionary by using a for loop.
+
+# When looping through a dictionary, the return value are the keys of the dictionary, but there are methods to return the values as well.
+
+# Print all key names in the dictionary, one by one:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+for x in thisdict:
+  print(x)
+
+# Print all values in the dictionary, one by one:
+
+for x in thisdict:
+  print(thisdict[x])
+
+# You can also use the values() method to return values of a dictionary:
+
+for x in thisdict.values():
+  print(x)
+
+# You can use the keys() method to return the keys of a dictionary:
+
+for x in thisdict.keys():
+  print(x)
+
+# Loop through both keys and values, by using the items() method:
+
+for x, y in thisdict.items():
+  print(x, y)
