97df5aaa46728b481800756300ce60f4b4e535f2
lin6echo <bajzathcsaba@gmail.com>
2022-08-30 18:21:46 +0200

    1

--- python/143.if.else.py
+++ python/143.if.else.py
@@ -1,13 +1,13 @@
 #The else keyword catches anything which isn't caught by the preceding conditions.
 
-# a = 200
-# b = 33
-# if b > a:
-#   print("b is greater than a")
-# elif a == b:
-#   print("a and b are equal")
-# else:
-#   print("a is greater than b")
+a = 200
+b = 33
+if b > a:
+   print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
 
 #You can also have an else without the elif:
 
--- python/high.score.py
+++ python/high.score.py
@@ -1,6 +1,8 @@
 def divide(n1, n2):
     result = n1 / n2
-    
+
+
 divide(10, 5)
+
 
 divide(n2 = 5, n1 = 10)
