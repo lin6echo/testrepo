042aa2688aa1ae15e588cdf939a474fb64fa21d6
lin6echo <bajzathcsaba@gmail.com>
2022-09-08 20:28:16 +0200

    1

---
+++ python/187.regular.expression.py
@@ -0,0 +1,7 @@
+import re
+
+split_term = "@"
+
+email = "user@gmail.com"
+
+print(re.split(split_term, email))
---
+++ python/192.forloop.in.dictionary.py
@@ -0,0 +1,26 @@
+mydict = {"name" : "Csaba", "wife": "Szilvia", "firstchild": "Zsani", "secondchild": "Bogi" }
+print(mydict)
+
+for key in mydict:
+    print(key)
+    
+for key in mydict.keys():
+    print(key)
+    
+for value in mydict.values():
+    print(value)
+    
+for key, value in mydict.items():
+    print(key, value)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
